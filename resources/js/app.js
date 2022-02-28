c8d35912cc5f4a9d7c825b77df521a309f90e43d
先規　勇樹 <yukisaki80@gmail.com>
2022-02-28 11:06:49 +0900

    routerの記述を切り出す

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,11 @@
  */
 import VueRouter from "vue-router";
 import HeaderComponent from "./components/HeaderComponent";
-import TaskListComponent from "./components/TaskListComponent";
-import TaskShowComponent from "./components/TaskShowComponent";
-import TaskCreateComponent from "./components/TaskCreateComponent";
-import TaskEditComponent from "./components/TaskEditComponent";
+// import TaskListComponent from "./components/TaskListComponent";
+// import TaskShowComponent from "./components/TaskShowComponent";
+// import TaskCreateComponent from "./components/TaskCreateComponent";
+// import TaskEditComponent from "./components/TaskEditComponent";
+import router from "./router.js"
 
 require("./bootstrap");
 
@@ -38,33 +39,33 @@ Vue.component("header-component", HeaderComponent);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const router = new VueRouter({
-    mode: "history",
-    routes: [
-        {
-            path: "/tasks",
-            name: "task.list",
-            component: TaskListComponent,
-        },
-        {
-            path: "/tasks/:taskId",
-            name: "task.show",
-            component: TaskShowComponent,
-            props: true,
-        },
-        {
-            path: "/tasks/create",
-            name: "task.create",
-            component: TaskCreateComponent,
-        },
-        {
-            path: "/tasks/:taskId/edit",
-            name: "task.edit",
-            component: TaskEditComponent,
-            props: true,
-        },
-    ],
-});
+// const router = new VueRouter({
+//     mode: "history",
+//     routes: [
+//         {
+//             path: "/tasks",
+//             name: "task.list",
+//             component: TaskListComponent,
+//         },
+//         {
+//             path: "/tasks/:taskId",
+//             name: "task.show",
+//             component: TaskShowComponent,
+//             props: true,
+//         },
+//         {
+//             path: "/tasks/create",
+//             name: "task.create",
+//             component: TaskCreateComponent,
+//         },
+//         {
+//             path: "/tasks/:taskId/edit",
+//             name: "task.edit",
+//             component: TaskEditComponent,
+//             props: true,
+//         },
+//     ],
+// });
 
 const app = new Vue({
     router,
